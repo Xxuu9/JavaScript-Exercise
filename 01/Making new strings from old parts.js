9d7dd57c181dffb7e9c346b94f4eea3754e54e5c
Xiangxu Teng <tengxxu@gmail.com>
2021-05-05 22:28:32 -0400

    Add MakeNewStrings

--- 01/Making new strings from old parts.js
+++ 01/Making new strings from old parts.js
@@ -1,21 +1,25 @@
 
+var list = document.querySelector('ul');
 
-var list = ['MAN675847583748sjt567654;Manchester Piccadilly',
+
+var list1 = ['MAN675847583748sjt567654;Manchester Piccadilly',
                 'GNF576746573fhdg4737dh4;Greenfield',
                 'LIV5hg65hd737456236dch46dg4;Liverpool Lime Street',
                 'SYB4f65hf75f736463;Stalybridge',
                 'HUD5767ghtyfyr4536dh45dg45dg3;Huddersfield'];
+
+
 function fixing(){
-    for(var i = 0; i < list.length; i++){
+    for(var i = 0; i < list1.length; i++){
         
-        var firstThreeLetters = list[i].slice(0,2);
-        let semicolonNumber = list[i].indexOf(';');
-        var stationName = list[i].slice(semicolonNumber+1);
+        var firstThreeLetters = list1[i].slice(0,2);
+        let semicolonNumber = list1[i].indexOf(';');
+        var stationName = list1[i].slice(semicolonNumber+1);
         var input = firstThreeLetters +':' + stationName;
         var result = input;
-        let para = document.createElement('h1');
-        para.textContent = result;
-        document.body.appendChild(para);
+        var listItem = document.createElement('li');
+        listItem.textContent = result;
+        list.appendChild(listItem);
 }
 }
 
