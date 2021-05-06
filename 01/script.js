8f5d63a754e501a3babfef73471036318942d6ed
Xiangxu Teng <tengxxu@gmail.com>
2021-05-05 20:49:30 -0400

    Add capitalization

--- 01/script.js
+++ 01/script.js
@@ -2,18 +2,42 @@ let greetings = ['Happy Birthday!','Merry Christmas my love',
 'A happy Christmas to all the family','You\'re all I want for Christmas',
 'Get well soon'];
 
-function filterGreetings(){
-for (let i = 0; i < greetings.length; i++) {
-    if(greetings[i].indexOf('Christmas') !== -1){
-    console.log(greetings[i]);
-    let para = document.createElement('h1');
-    para.textContent = greetings[i];
-    document.body.appendChild(para);
-}
-}
-}
-let btn = document.querySelector('button');
+let cities = ['lonDon', 'ManCHESTer', 'BiRmiNGHAM', 'liVERpoOL', 'SooS']
 
-btn.addEventListener('click',filterGreetings);
+function filterGreetings(){
+    let result;
+    for (let i = 0; i < greetings.length; i++) {
+        if(greetings[i].indexOf('Christmas') !== -1){
+            console.log(greetings[i]);
+            let para = document.createElement('h1');
+            result = greetings[i];
+            para.textContent = result;
+            document.body.appendChild(para);
+            }
+        }
+}
+
+function capitalization() {
+    let lowerCityName;
+    let upperFirstChar;
+    let result;
+    for (let i = 0; i < cities.length; i++) {
+        lowerCityName = cities[i].toLowerCase();
+        upperFirstChar = cities[i][0].toUpperCase();
+        result = lowerCityName.replace(lowerCityName[0], upperFirstChar);
+        // console.log(lowerCityName, upperFirstChar, result);
+        let para = document.createElement('h1');
+        para.textContent = result;
+        document.body.appendChild(para);
+    }
+
+}
+
+
+let btn1 = document.querySelector('.filterGreetings');
+let btn2 = document.querySelector('.capitalization');
+
+btn1.addEventListener('click', filterGreetings);
+btn2.addEventListener('click', capitalization);
 
 
