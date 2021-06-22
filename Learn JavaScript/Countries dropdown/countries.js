dd05dad4c3d1b8ce7c33a5d8bfc73b54182c3e7c
Sophie <tengxxu@gmail.com>
2021-06-22 10:58:25 -0400

    create a dropdown js

---
+++ Learn JavaScript/Countries dropdown/countries.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string[]} countries
+ */
+ export const getDropdown = (countries) => {
+    console.log(countries);
+    let html = `<option value="">Please select</option>`
+    countries.forEach(country => {
+        html += `<option value="${country.toLowerCase()}">${country}</option>`
+    })
+    return html;
+}
